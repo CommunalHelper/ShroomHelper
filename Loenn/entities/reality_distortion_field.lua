7672da21a85bca87fb18e6a74db4e9c25e54953d
snomii <68405838+snomii@users.noreply.github.com>
2022-09-20 21:01:05 -0600

    RealityDistortionField Support
    Added RealityDistortionField plugin

---
+++ Loenn/entities/reality_distortion_field.lua
@@ -0,0 +1,16 @@
+local reality_distortion_field = {}
+
+reality_distortion_field.name = "ShroomHelper/RealityDistortionField"
+reality_distortion_field.depth = 0
+reality_distortion_field.fillColor = {0.0, 0.0, 1.0}
+reality_distortion_field.borderColor = {0.0, 0.0, 1.0}
+reality_distortion_field.placements = {
+    name = "reality_distortion_field",
+    data = {
+        width = 8,
+        height = 8,
+        rippleAreaMultiplier = 1.0
+    }
+}
+
+return reality_distortion_field
