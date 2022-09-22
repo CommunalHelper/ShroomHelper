2bef0716f721e111881454729c07df60eb124e07
snomii <68405838+snomii@users.noreply.github.com>
2022-09-22 17:51:24 -0600

    Shroom Petals Effect Support
    Added Ahorn + Loenn tooltips for shroom petals effect
    Added Loenn support for shroom petals effect

---
+++ Loenn/effects/shroom_petals.lua
@@ -0,0 +1,15 @@
+local celesteEnums = require("consts.celeste_enums")
+
+local shroom_petals = {}
+
+shroom_petals.name = "ShroomHelper/ShroomPetals"
+shroom_petals.fieldInformation = {
+    color = {
+        fieldType = "color",
+    }
+}
+shroom_petals.defaultData = {
+    color = "161933"
+}
+
+return shroom_petals
